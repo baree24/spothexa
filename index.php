4a26f7b5d112498d26d9068075e5e9e6be5faf9f
baree24 <72135643+baree24@users.noreply.github.com>
2023-08-08 10:47:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,326 @@
+<?php
+
+error_reporting(0);
+system ('clear');
+system ('figlet Auto Regist');
+system('xdg-open https://wa.me/6282140405001');
+date_default_timezone_set("Asia/Jakarta");
+$colors = new Colors();
+
+function bf4A70AF6432Aee($F0e9bd8a04bb46b, $E8917d5fec68908, $E00ce2c9d2fdca2, $fe60a54ae2df91a, $B55a0f4682664a0, $b2d56171a1a999e, $ac87d040ead0a2d)
+{
+    $Ae90d4abde78018 = curl_init();
+    curl_setopt($Ae90d4abde78018, CURLOPT_URL, $E00ce2c9d2fdca2);
+    curl_setopt($Ae90d4abde78018, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($Ae90d4abde78018, CURLOPT_FOLLOWLOCATION, false);
+    if (!($B55a0f4682664a0 == true)) {
+        goto Ea85ee26d50d063;
+    }
+    curl_setopt($Ae90d4abde78018, CURLOPT_POST, 1);
+    curl_setopt($Ae90d4abde78018, CURLOPT_POSTFIELDS, $F0e9bd8a04bb46b);
+    Ea85ee26d50d063:
+    curl_setopt($Ae90d4abde78018, CURLOPT_ENCODING, "GZIP");
+    if (!($fe60a54ae2df91a == true)) {
+        goto B4c8e6a9dc59e6d;
+    }
+    curl_setopt($Ae90d4abde78018, CURLOPT_CUSTOMREQUEST, $ac87d040ead0a2d);
+    B4c8e6a9dc59e6d:
+    curl_setopt($Ae90d4abde78018, CURLOPT_HTTPHEADER, $E8917d5fec68908);
+    if (!($b2d56171a1a999e == true)) {
+        goto a320d485fffc708;
+    }
+    curl_setopt($Ae90d4abde78018, CURLOPT_HEADER, 1);
+    a320d485fffc708:
+    $ade9b5f5d1a56c7 = curl_exec($Ae90d4abde78018);
+    if (!curl_errno($Ae90d4abde78018)) {
+        goto ee1b08c1d4544d1;
+    }
+    echo "Error:" . curl_error($Ae90d4abde78018);
+    ee1b08c1d4544d1:
+    curl_close($Ae90d4abde78018);
+    return $ade9b5f5d1a56c7;
+}
+function getRandomMail()
+{
+    $D7090a9cae84ebd = array("hexagoo.shop");
+    $E7bcae87d095fe7 = array_rand($D7090a9cae84ebd, true);
+    return $D7090a9cae84ebd[$E7bcae87d095fe7];
+}
+function C423bF2E8B8d153($F58a433c7b3f0de)
+{
+    $E00ce2c9d2fdca2 = "http://ninjaname.horseridersupply.com/indonesian_name.php";
+    $F0e9bd8a04bb46b = "number_generate=1&gender_type=male&submit=Generate";
+    $E8917d5fec68908 = array();
+    $E8917d5fec68908[] = "Content-Length: " . strlen($F0e9bd8a04bb46b);
+    $E8917d5fec68908[] = "User-Agent: " . $F58a433c7b3f0de;
+    $E8917d5fec68908[] = "Content-Type: application/x-www-form-urlencoded";
+    $F277aa80ab1fe14 = bf4a70Af6432AEe($F0e9bd8a04bb46b, $E8917d5fec68908, $E00ce2c9d2fdca2, $fe60a54ae2df91a = null, $B55a0f4682664a0 = true, $b2d56171a1a999e = false, $ac87d040ead0a2d = null);
+    $b10993f84d0b6d5 = explode("&bull;", $F277aa80ab1fe14)[1];
+    return explode("<br/>", $b10993f84d0b6d5)[0];
+}
+function B8444DD767B344C()
+{
+    $F5cb0241672a692 = array("Mozilla", "Firefox", "Explorer");
+    $E7bcae87d095fe7 = array_rand($F5cb0241672a692, true);
+    $F5cb0241672a692 = $F5cb0241672a692[$E7bcae87d095fe7];
+    $b454687590546ca = array("4.0", "4.4", "5.0", "6.0", "5.5", "7.0", "8", "9", "10", "11", "12", "13");
+    $E7bcae87d095fe7 = array_rand($b454687590546ca, true);
+    $b454687590546ca = $b454687590546ca[$E7bcae87d095fe7];
+    $B014673119d792d = array("Windows", "Linux");
+    $E7bcae87d095fe7 = array_rand($B014673119d792d, true);
+    $B014673119d792d = $B014673119d792d[$E7bcae87d095fe7];
+    $d89f47d0e6e1175 = substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"), 0, 7);
+    $Fb2c049bbd4e169 = rand(11, 99) . "." . rand(1, 9) . "." . rand(1111, 9999) . "." . rand(10, 99);
+    $C4091fea5e41fe4 = rand(111, 999) . "." . rand(11, 99);
+    $c69e73171f14306 = rand(1, 9) . "." . rand(0, 9);
+    return $F58a433c7b3f0de = "{$F5cb0241672a692}/{$c69e73171f14306} ({$B014673119d792d}; Android {$b454687590546ca}; {$d89f47d0e6e1175}) AppleWebKit/{$C4091fea5e41fe4} (KHTML, like Gecko) Chrome/{$Fb2c049bbd4e169} Mobile Safari/{$C4091fea5e41fe4}";
+}
+function EbEaDEcE433011d($F2f9687bae2b194)
+{
+    if (file_exists("config.json")) {
+        $b89e2db36a55ca3 = file_get_contents("config.json");
+        return json_decode($b89e2db36a55ca3, true);
+    }
+    system("touch config.json");
+    echo " {$F2f9687bae2b194} !! Write file config.json\n";
+    die;
+}
+function f321ED4cd36b631($F58a433c7b3f0de)
+{
+    $E00ce2c9d2fdca2 = "https://accounts.spotify.com/en/login";
+    $ac87d040ead0a2d = "GET";
+    $E8917d5fec68908 = array();
+    $E8917d5fec68908[] = "User-Agent: " . $F58a433c7b3f0de;
+    $E8917d5fec68908[] = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3";
+    $E8917d5fec68908[] = "Accept-Encoding: gzip, deflate, br";
+    $E8917d5fec68908[] = "Accept-Language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7";
+    $E8917d5fec68908[] = "Cookie: __Host-device_id=AQASTaFlcfRwTXhko6Y5HZmzSochQ7qB8ghzHLlyBgthfH9Rv8iTUtZ8lf0hHvhQmbGdaN_E8CDrhttjU88OBKO0DzZZE7nTlTM;sp_sso_csrf_token=013acda7193cecfaa271306f303c2b43f45eb6380431363732343535393236303530;__Host-sp_csrf_sid=b7eb719dbaf5cf8c9e5eec8888cc25a813c37db179091fc3d2d324f2d70143bd";
+    return BF4a70AF6432aEE($F0e9bd8a04bb46b = null, $E8917d5fec68908, $E00ce2c9d2fdca2, $fe60a54ae2df91a = true, $B55a0f4682664a0 = false, $b2d56171a1a999e = true, $ac87d040ead0a2d = "GET");
+}
+function cdD6b1c909589f6($E4bc9ba42437ae0, $E852fc7195cc8f5, $bdd26c32ee961f6, $E73a70d1d24ab85, $e54a09989408a03, $F58a433c7b3f0de, $dfc7a2d7ee1d2b5, $A879e78225b35a4)
+{
+    $E00ce2c9d2fdca2 = "https://spclient.wg.spotify.com/signup/public/v2/account/create";
+    $F0e9bd8a04bb46b = "{\"account_details\":{\"birthdate\":\"" . $bdd26c32ee961f6 . "\",\"consent_flags\":{\"eula_agreed\":true,\"send_email\":false,\"third_party_email\":true},\"display_name\":\"" . $E73a70d1d24ab85 . "\",\"email_and_password_identifier\":{\"email\":\"" . $E4bc9ba42437ae0 . "\",\"password\":\"" . $E852fc7195cc8f5 . "\"},\"gender\":" . $dfc7a2d7ee1d2b5 . "},\"callback_uri\":\"https://www.spotify.com/signup/challenge?locale=id\",\"client_info\":{\"api_key\":\"" . $A879e78225b35a4 . "\",\"app_version\":\"v2\",\"capabilities\":[1],\"installation_id\":\"" . $e54a09989408a03 . "\",\"platform\":\"www\"},\"tracking\":{\"creation_flow\":\"\",\"creation_point\":\"https://www.spotify.com/id/premium/\",\"referrer\":\"checkout\"},\"recaptcha_token\":\"null\"}";
+    $E8917d5fec68908 = array();
+    $E8917d5fec68908[] = "Content-Length: " . strlen($F0e9bd8a04bb46b);
+    $E8917d5fec68908[] = "User-Agent: " . $F58a433c7b3f0de;
+    $E8917d5fec68908[] = "Content-Type: application/json";
+    $E8917d5fec68908[] = "Accept: */*";
+    $E8917d5fec68908[] = "Accept-Language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7";
+    return Bf4A70Af6432AeE($F0e9bd8a04bb46b, $E8917d5fec68908, $E00ce2c9d2fdca2, $fe60a54ae2df91a = null, $B55a0f4682664a0 = true, $b2d56171a1a999e = false, $ac87d040ead0a2d = null);
+}
+function eaFFA67e37720b5($b35a815e837dc97, $Ccb7278da8f037f, $F48e27c53ae480a, $F58a433c7b3f0de)
+{
+    $E00ce2c9d2fdca2 = "https://www.spotify.com/api/signup/authenticate";
+    $F0e9bd8a04bb46b = "splot={$b35a815e837dc97}";
+    $E8917d5fec68908 = array();
+    $E8917d5fec68908[] = "Content-Length: " . strlen($F0e9bd8a04bb46b);
+    $E8917d5fec68908[] = "X-Csrf-Token: " . $Ccb7278da8f037f;
+    $E8917d5fec68908[] = "User-Agent: " . $F58a433c7b3f0de;
+    $E8917d5fec68908[] = "Content-Type: application/x-www-form-urlencoded";
+    $E8917d5fec68908[] = "Accept: */*";
+    $E8917d5fec68908[] = "Accept-Encoding: gzip, deflate, br";
+    $E8917d5fec68908[] = "Accept-Language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7";
+    $E8917d5fec68908[] = "Cookie: __Host-sp_csrf_sid=" . $F48e27c53ae480a;
+    return bF4A70AF6432aEE($F0e9bd8a04bb46b, $E8917d5fec68908, $E00ce2c9d2fdca2, $fe60a54ae2df91a = null, $B55a0f4682664a0 = true, $b2d56171a1a999e = true, $ac87d040ead0a2d = null);
+}
+function F45700Afc18415B($e4ca3e549bd72f8)
+{
+    $E00ce2c9d2fdca2 = "https://open.spotify.com/get_access_token?reason=transport&productType=web_player";
+    $ac87d040ead0a2d = "GET";
+    $E8917d5fec68908 = array();
+    $E8917d5fec68908[] = "User-Agent: Mozilla/5.0 (Linux; Android 10; RMX2061) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.186 Mobile Safari/537.36";
+    $E8917d5fec68908[] = "Content-Type: application/x-www-form-urlencoded";
+    $E8917d5fec68908[] = "Accept: */*";
+    $E8917d5fec68908[] = "Accept-Encoding: gzip, deflate, br";
+    $E8917d5fec68908[] = "Accept-Language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7";
+    $E8917d5fec68908[] = "cookie: sp_dc=" . $e4ca3e549bd72f8;
+    return Bf4A70aF6432aeE($F0e9bd8a04bb46b = null, $E8917d5fec68908, $E00ce2c9d2fdca2, $fe60a54ae2df91a = true, $B55a0f4682664a0 = false, $b2d56171a1a999e = false, $ac87d040ead0a2d = "GET");
+}
+function B65DE1d554a85eA($d68527f5870bccb, $e0b44881782a222, $c86ad085a70f258, $A8514793a1023f4, $d80f344a00a7c27)
+{
+    $E00ce2c9d2fdca2 = "https://spclient.wg.spotify.com/premium-destination-hubs/v2/page?locale=id&device_id={$A8514793a1023f4}&partner_id=&referrer_id=&build_model={$c86ad085a70f258}&cache_key=free&show_unsafe_unpublished_content=false&manufacturer=samsung";
+    $ac87d040ead0a2d = "GET";
+    $E8917d5fec68908 = array();
+    $E8917d5fec68908[] = "Accept-Language: id-ID;q=1, en-US;q=0.5";
+    $E8917d5fec68908[] = "User-Agent: Spotify/8.8.14.575 Android/30 " . $d80f344a00a7c27;
+    $E8917d5fec68908[] = "Spotify-App-Version: 8.8.14.575";
+    $E8917d5fec68908[] = "X-Client-Id: 9a8d2f0ce77a4e248bb71fefcb557637";
+    $E8917d5fec68908[] = "App-Platform: Android";
+    $E8917d5fec68908[] = "Client-Token: " . $e0b44881782a222;
+    $E8917d5fec68908[] = "Authorization: Bearer " . $d68527f5870bccb;
+    $E8917d5fec68908[] = "Accept-Encoding: gzip";
+    return bf4A70Af6432Aee($F0e9bd8a04bb46b = null, $E8917d5fec68908, $E00ce2c9d2fdca2, $fe60a54ae2df91a = true, $B55a0f4682664a0 = false, $b2d56171a1a999e = false, $ac87d040ead0a2d = "GET");
+}
+function e6715Df03b351A6()
+{
+    $E00ce2c9d2fdca2 = "https://clienttoken.spotify.com/v1/clienttoken";
+    $F0e9bd8a04bb46b = "{\"client_data\":{\"client_version\":\"1.2.10.347.gdfd7b317\",\"client_id\":\"d8a5ed958d274c2e8ee717e6a4b0971d\",\"js_sdk_data\":{\"device_brand\":\"samsung\",\"device_model\":\"SM-G99B\",\"os\":\"windows\",\"os_version\":\"12\",\"device_id\":\"d91816d8-6ae1-4c21-a64d-a7fe7e073ec7\",\"device_type\":\"mobile\"}}}";
+    $E8917d5fec68908 = array();
+    $E8917d5fec68908[] = "Content-Length: " . strlen($F0e9bd8a04bb46b);
+    $E8917d5fec68908[] = "Accept: application/json";
+    $E8917d5fec68908[] = "User-Agent: Spotify/8.8.14.575 Android/30 SM-G99B";
+    $E8917d5fec68908[] = "Content-Type: application/json";
+    return Bf4a70aF6432AeE($F0e9bd8a04bb46b, $E8917d5fec68908, $E00ce2c9d2fdca2, $fe60a54ae2df91a = null, $B55a0f4682664a0 = true, $b2d56171a1a999e = false, $ac87d040ead0a2d = null);
+}
+function a97Ea3dc2ED94aB()
+{
+    return sprintf("%04x%04x-%04x-%04x-%04x-%04x%04x%04x", mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xfff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000, mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff));
+}
+function c9088206F16636b()
+{
+    return sprintf("%04x%04x%04x%04x", mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xfff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000, mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff));
+}
+function C5A1dE21430367E($F2f9687bae2b194, $F14f9254b2214d6)
+{
+    A01e8b1569e29a2:
+    $Ca6f8e1525773ed = ebEADece433011D($F2f9687bae2b194);
+    $E852fc7195cc8f5 = $Ca6f8e1525773ed["Spotify"]["Password"];
+    $A879e78225b35a4 = $Ca6f8e1525773ed["Spotify"]["Config"];
+    $F58a433c7b3f0de = B8444Dd767b344C();
+    $c46aafb8eb9b5c0 = rand(1, 12);
+    $C919d1240781a74 = rand(1, 30);
+    $Ab3c1b0f051e6c5 = $c46aafb8eb9b5c0 < 10 ? $c46aafb8eb9b5c0 = "0" . $c46aafb8eb9b5c0 : ($c46aafb8eb9b5c0 = $c46aafb8eb9b5c0);
+    $B923951d206ebcf = $C919d1240781a74 < 10 ? $C919d1240781a74 = "0" . $C919d1240781a74 : ($C919d1240781a74 = $C919d1240781a74);
+    $ed123250747987f = rand(1980, 2003);
+    $bdd26c32ee961f6 = "{$ed123250747987f}-{$Ab3c1b0f051e6c5}-{$B923951d206ebcf}";
+    $e54a09989408a03 = A97eA3dC2ED94AB();
+    $dfc7a2d7ee1d2b5 = rand(1, 4);
+    $Addc1e08d40654f = cDd6b1C909589F6($E4bc9ba42437ae0, $E852fc7195cc8f5, $bdd26c32ee961f6, $E73a70d1d24ab85, $e54a09989408a03, $F58a433c7b3f0de, $dfc7a2d7ee1d2b5, $A879e78225b35a4);
+    $E73a70d1d24ab85 = c423BF2e8b8d153($F58a433c7b3f0de);
+    $bcbcab320bc7784 = getRandomMail();
+    $a03350491430291 = str_replace(" ", '', $E73a70d1d24ab85);
+    $E4bc9ba42437ae0 = "{$a03350491430291}@{$bcbcab320bc7784}";
+    echo "\n \xe2\xaa\xa9 {$E4bc9ba42437ae0} + {$E852fc7195cc8f5}\n";
+    $Addc1e08d40654f = cdd6b1C909589F6($E4bc9ba42437ae0, $E852fc7195cc8f5, $bdd26c32ee961f6, $E73a70d1d24ab85, $e54a09989408a03, $F58a433c7b3f0de, $dfc7a2d7ee1d2b5, $A879e78225b35a4);
+    if (preg_match("/login_token/i", $Addc1e08d40654f)) {
+        $ce4f205cf541654 = json_decode($Addc1e08d40654f, true);
+        $b35a815e837dc97 = $ce4f205cf541654["success"]["login_token"];
+        echo "{$F14f9254b2214d6} \xe2\xaa\xa9 Sukses Daftar Aplikasi Spotify ";
+        b9079c1f0ccb5b4:
+        $c3b5389c266697b = F321eD4cD36b631($F58a433c7b3f0de);
+        preg_match_all("/^Set-Cookie:\\s*([^;]*)/mi", $c3b5389c266697b, $dd5a9d9c6ed2a33);
+        $e16de947ba1c60d = array();
+        foreach ($dd5a9d9c6ed2a33[1] as $e353f322ee3e12a) {
+            parse_str($e353f322ee3e12a, $f95a985cd70e773);
+            $e16de947ba1c60d = array_merge($e16de947ba1c60d, $f95a985cd70e773);
+        }
+        $c3b5389c266697b = F321Ed4cD36b631($F58a433c7b3f0de);
+        preg_match_all("/^Set-Cookie:\\s*([^;]*)/mi", $c3b5389c266697b, $dd5a9d9c6ed2a33);
+        $e16de947ba1c60d = array();
+        foreach ($dd5a9d9c6ed2a33[1] as $e353f322ee3e12a) {
+            parse_str($e353f322ee3e12a, $f95a985cd70e773);
+            $e16de947ba1c60d = array_merge($e16de947ba1c60d, $f95a985cd70e773);
+        }
+        $Ccb7278da8f037f = $e16de947ba1c60d["sp_sso_csrf_token"];
+        $F48e27c53ae480a = $e16de947ba1c60d["__Host-sp_csrf_sid"];
+        $Dadfdf44b0782dd = eaffA67e37720B5($b35a815e837dc97, $Ccb7278da8f037f, $F48e27c53ae480a, $F58a433c7b3f0de);
+        preg_match("/sp_dc=([^;]+)/", $Dadfdf44b0782dd, $dd5a9d9c6ed2a33);
+        $e4ca3e549bd72f8 = $dd5a9d9c6ed2a33[1];
+        if (!($e4ca3e549bd72f8 == null)) {
+            $b9fca960539e9f7 = f45700AfC18415B($e4ca3e549bd72f8);
+            $ce4f205cf541654 = json_decode($b9fca960539e9f7, true);
+            if (!($ce4f205cf541654["isAnonymous"] == false)) {
+                goto Ae34e0b213dccbd;
+            }
+            $d68527f5870bccb = $ce4f205cf541654["accessToken"];
+            Ae34e0b213dccbd:
+            $bc0b626e91b3ad0 = e6715dF03b351a6();
+            $C16d2b5abb58318 = json_decode($bc0b626e91b3ad0, true);
+            $e0b44881782a222 = $C16d2b5abb58318["granted_token"]["token"];
+            $A8514793a1023f4 = c9088206f16636b();
+            $be0db6e616b0d25 = "SM-" . substr(str_shuffle("ABCEEFGIHJKLMNOPQRSTUVWXYZ0123456789"), 0, 5);
+            $d80f344a00a7c27 = "({$be0db6e616b0d25})";
+            $f1c4747c2c7f131 = B65dE1d554A85Ea($d68527f5870bccb, $e0b44881782a222, $c86ad085a70f258, $A8514793a1023f4, $d80f344a00a7c27);
+            echo "\n \xe2\xaa\xa9 Sukses Klaim Promo Samsung \n";
+            sleep(5);
+            file_put_contents("acc.txt", "{$E4bc9ba42437ae0}|{$E852fc7195cc8f5}\n", FILE_APPEND);
+            // [PHPDeobfuscator] Implied return
+            return;
+        }
+        echo "\n{$F2f9687bae2b194} \xe2\xaa\xa9 Gagal mengambil token\n\n";
+        die;
+    }
+    if (preg_match("/already_exists/i", $Addc1e08d40654f)) {
+        echo "{$F2f9687bae2b194} \xe2\xaa\xa9 Sudah ada akun untuk email ini.\n\n{$F14f9254b2214d6}";
+        goto A01e8b1569e29a2;
+    }
+    var_dump("\n\n{$Addc1e08d40654f}\n\n");
+    die;
+}
+echo PHP_EOL.'>> '.$colors->getColoredString("AUTO REGIST ACCOUNT SPOTIFY CLAIM OFFER SAMSUNG", "green").' <<'.PHP_EOL.PHP_EOL;
+echo '             '.$colors->getColoredString("MADE FROM IMAM BARI - 2023", "green").' '.PHP_EOL.PHP_EOL;
+echo '[ '.date('H:i:s').' ] Berapa akun : ';
+$d78c26868be85ba = trim(fgets(STDIN));
+$F926a0f5b2ff815 = 0;
+b8809bda4a3a3be:
+if (!($F926a0f5b2ff815 < $d78c26868be85ba)) {
+    return;
+}
+echo PHP_EOL.'------------------ '.$colors->getColoredString("DETAIL ACCOUNT", "green").' -----------------'.PHP_EOL;
+class Colors {
+    private $foreground_colors = array();
+    private $background_colors = array();
+
+    public function __construct() {
+        // Set up shell colors
+        $this->foreground_colors['black'] = '0;30';
+        $this->foreground_colors['dark_gray'] = '1;30';
+        $this->foreground_colors['blue'] = '0;34';
+        $this->foreground_colors['light_blue'] = '1;34';
+        $this->foreground_colors['green'] = '0;32';
+        $this->foreground_colors['light_green'] = '1;32';
+        $this->foreground_colors['cyan'] = '0;36';
+        $this->foreground_colors['light_cyan'] = '1;36';
+        $this->foreground_colors['red'] = '0;31';
+        $this->foreground_colors['light_red'] = '1;31';
+        $this->foreground_colors['purple'] = '0;35';
+        $this->foreground_colors['light_purple'] = '1;35';
+        $this->foreground_colors['brown'] = '0;33';
+        $this->foreground_colors['yellow'] = '1;33';
+        $this->foreground_colors['light_gray'] = '0;37';
+        $this->foreground_colors['white'] = '1;37';
+
+        $this->background_colors['black'] = '40';
+        $this->background_colors['red'] = '41';
+        $this->background_colors['green'] = '42';
+        $this->background_colors['yellow'] = '43';
+        $this->background_colors['blue'] = '44';
+        $this->background_colors['magenta'] = '45';
+        $this->background_colors['cyan'] = '46';
+        $this->background_colors['light_gray'] = '47';
+    }
+
+    // Returns colored string
+    public function getColoredString($string, $foreground_color = null, $background_color = null) {
+        $colored_string = "";
+
+        // Check if given foreground color found
+        if (isset($this->foreground_colors[$foreground_color])) {
+            $colored_string .= "\033[" . $this->foreground_colors[$foreground_color] . "m";
+        }
+        // Check if given background color found
+        if (isset($this->background_colors[$background_color])) {
+            $colored_string .= "\033[" . $this->background_colors[$background_color] . "m";
+        }
+
+        // Add string and end coloring
+        $colored_string .=  $string . "\033[0m";
+
+        return $colored_string;
+    }
+
+    // Returns all foreground color names
+    public function getForegroundColors() {
+        return array_keys($this->foreground_colors);
+    }
+
+    // Returns all background color names
+    public function getBackgroundColors() {
+        return array_keys($this->background_colors);
+    }
+}
+
+$F277aa80ab1fe14 = c5A1De21430367e($F2f9687bae2b194, $F14f9254b2214d6);
+$F926a0f5b2ff815++;
+goto b8809bda4a3a3be;
